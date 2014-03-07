08f1593164f3bbc4ff4c862f6473f3f07e05e1e1
rdbwebster <rdbwebster@gmail.com>
2014-03-07 06:09:32 -0800

    Inital version

---
+++ ManageCompositesApp/CompleteCompositeInstances/deploy/run.sh
@@ -0,0 +1,19 @@
+# This script should be in the same directory as the ManageComposite.jar file
+
+# Modify the FMW_HOME to point to local location of oracle fmw
+FMW_HOME=/oracle/fmwhome
+
+CLASSPATH=./ManageComposites.jar
+CLASSPATH=$CLASSPATH:$FMW_HOME/oracle_common/soa/modules/oracle.soa.mgmt_11.1.1/soa-infra-mgmt.jar
+CLASSPATH=$CLASSPATH:$FMW_HOME/oracle_common/webservices/wsclient_extended.jar
+CLASSPATH=$CLASSPATH:$FMW_HOME/oracle_common/modules/oracle.fabriccommon_11.1.1/fabric-common.jar
+CLASSPATH=$CLASSPATH:$FMW_HOME/oracle_common/modules/wlthint3client.jar
+
+# Check the input parameters on the last line
+# p1 - listFaulted or removeFaulted
+# p2 - SOA partition name
+# p3 - SOA Server host name
+# p4 - SOA Server port
+# p5 - SOA Admin username
+# p6 - SOA Admin password
+java -cp $CLASSPATH com.managesoa.ManageComposites listFaulted default localhost 7001 weblogic welcome1
